d418002ae8296e379c994b70032a03eac3ce58d6
osmn-byhn <osmanbeyhan12@gmail.com>
2025-02-22 16:13:19 +0300

    commit for edit sidebar

--- bin/cli.js
+++ bin/cli.js
@@ -1,3 +1,10 @@
 #!/usr/bin/env node
 
-import("../index.js");
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+import(`${__dirname}/../index.js`).catch((err) => {
+  console.error("Modül yüklenirken hata oluştu:", err);
+  process.exit(1);
+});
